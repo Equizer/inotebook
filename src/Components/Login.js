f111cf983f53011256aaae210e4da3154e1112eb
Equizer <130571773+Equizer@users.noreply.github.com>
2023-11-22 23:21:01 +0530

    complete the login form and redirect to home page if success is true

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,9 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom' //note that useHistory hook has been replaced with useNavigate hook after version 6 of react-touter-dom and now we dont need to use .() to use this hook we can just do it like that navigate(path) if we stored the hook in the variable named navigate
 
 const Login = () => {
-  const [credentials, setCredentials] = useState({email: "", password: ""})
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
   const hadleSubmit = async (event) => {
     event.preventDefault();
     const response = await fetch("http://localhost:5000/api/auth/login", {
@@ -9,12 +11,14 @@ const Login = () => {
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify({email: credentials.email, password: credentials.password})
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
     });
     const json = await response.json();
     console.log(json);
     if (json.success) {
-      //redirect
+      //save the auth token and redirect
+      localStorage.setItem('token', json.authtoken);
+      navigate('/');
     }
     else {
       alert("Invalid email or password");
@@ -23,11 +27,11 @@ const Login = () => {
 
 
   const onChange = (event) => {
-    setCredentials({...credentials, [event.target.name] : event.target.value});
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
   }
   return (
     <div>
-      <form  onSubmit={hadleSubmit}>
+      <form onSubmit={hadleSubmit}>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">Email address</label>
           <input type="email" className="form-control" value={credentials.email} id="email" aria-describedby="emailHelp" name="email" onChange={onChange} />
@@ -35,7 +39,7 @@ const Login = () => {
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" value={credentials.password} id="password" name="password" onChange={onChange}/>
+          <input type="password" className="form-control" value={credentials.password} id="password" name="password" onChange={onChange} />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
